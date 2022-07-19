db419d728eab0ca18a2549421c88ade75ee69d86
Phi Thai <s3878070@rmit.edu.vn>
2022-07-19 15:38:51 +0700

    Initial Commit

---
+++ EatX/EatXApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatXApp.swift
+//  EatX
+//
+//  Created by Phi Thai on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct EatXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
